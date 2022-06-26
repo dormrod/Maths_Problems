31d80c9b3c8eb62fb6850994f2ad57d9c609d38b
David Ormrod Morley <david.ormrodmorley@finbourne.com>
2022-06-26 22:57:11 +0100

    Add solutions to problems 1-5 in csharp

---
+++ csharp/ProjectEuler.UnitTests/TestHelpers.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace ProjectEuler.UnitTests
+{
+    [TestFixture]
+    public static class TestHelpers
+    {
+        /// <summary>
+        /// Generate Fibonacci sequence with values less than the provided cutoff.
+        /// </summary>
+        public static IEnumerable<(int Index, int Value)> FibonacciSequence(int cutoff = 10)
+        {
+            var firstTerm = 0;
+            var secondTerm = 1;
+            var i = 0;
+
+            yield return (++i, firstTerm);
+            yield return (++i, secondTerm);
+            
+            while (true)
+            {
+                var thirdTerm = firstTerm + secondTerm;
+                
+                if (thirdTerm > cutoff)
+                    yield break;
+
+                yield return (i++, thirdTerm);
+
+                firstTerm = secondTerm;
+                secondTerm = thirdTerm;
+            }
+        }
+        
+        /// <summary>
+        /// Generate Prime factors for value.
+        /// </summary>
+        public static IEnumerable<long> PrimeFactors(long value)
+        {
+            if (value <= 0)
+                throw new ArgumentException("Value must be greater than zero.");
+            
+            var currentValue = value;
+            var currentPrime = 2;
+            var primes = new List<long>();
+
+            while (true)
+            {
+                if (currentValue <= currentPrime)
+                {
+                    yield return currentValue;
+                    yield break;
+                }
+                
+                if (currentValue % currentPrime == 0)
+                {
+                    yield return currentPrime;
+                    currentValue /= currentPrime;
+                }
+                else
+                {
+                    NextPrime();
+                }
+            }
+
+            void NextPrime()
+            {
+                var nextPrime = currentPrime + 1;
+                while (true)
+                {
+                    var maxPossiblePrime = (long) Math.Sqrt(nextPrime);
+
+                    bool isPrime = true;
+                    foreach (var p in primes)
+                    {
+                        if (p > maxPossiblePrime)
+                            break;
+
+                        if (nextPrime % p == 0)
+                            isPrime = false;
+                    }
+
+                    if (isPrime)
+                    {
+                        currentPrime = nextPrime;
+                        primes.Add(currentPrime);
+                        return;
+                    }
+
+                    ++nextPrime;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Convert integer to an array of composite digits,
+        /// where the array index corresponds to the nth power of 10.
+        /// </summary>
+        public static IEnumerable<int> ToDigits(int value)
+        {
+            var remainder = value;
+            var digits = new List<int>();
+            
+            while (true)
+            {
+                var digit = remainder % 10;
+                remainder = (remainder - digit) / 10;
+                digits.Add(digit);
+                
+                if (remainder == 0)
+                    break;
+            }
+
+            return digits;
+        }
+    }
+}
---
+++ csharp/ProjectEuler.UnitTests/Tests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace ProjectEuler.UnitTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase(10, 23)]
+        [TestCase(1000, 233168)]
+        public void Problem1(int cutoff, int expected)
+        {
+            var answer = Enumerable.Range(0, cutoff)
+                .Where(i => i % 3 == 0 || i % 5 == 0)
+                .Sum();
+
+            Assert.That(answer, Is.EqualTo(expected));
+        }
+
+        [TestCase(10, 10)]
+        [TestCase(4000000, 4613732)]
+        public void Problem2(int cutoff, int expected)
+        {
+            var answer = TestHelpers.FibonacciSequence(cutoff)
+                .Where(x => x.Value % 2 == 0)
+                .Sum(x => x.Value);
+
+            Assert.That(answer, Is.EqualTo(expected));
+        }
+        
+        [TestCase(10, 5)]
+        [TestCase(100001, 9091)]
+        [TestCase(600851475143, 6857)]
+        public void Problem3(long value, int expected)
+        {
+            var answer = TestHelpers.PrimeFactors(value).Last();
+
+            Assert.That(answer, Is.EqualTo(expected));
+        }
+
+        [TestCase(99, 9009)]
+        [TestCase(999, 906609)]
+        public void Problem4(int max, int expected)
+        {
+            var answer = Enumerable.Range(1, max)
+                .SelectMany(i => Enumerable.Range(i, max - i + 1).Select(j => i * j))
+                .Select(v => new
+                {
+                    Value = v,
+                    Digits = TestHelpers.ToDigits(v)
+                })
+                .Where(x => x.Digits
+                    .Zip(
+                        x.Digits.Reverse(), 
+                        (i, j) => i == j)
+                    .All(b => b))
+                .Max(x => x.Value);
+
+            Assert.That(answer, Is.EqualTo(expected));
+        }
+        
+        [TestCase(10, 2520)]
+        [TestCase(20, 232792560)]
+        public void Problem5(int max, int expected)
+        {
+            var answer = Enumerable.Range(1, max)
+                .SelectMany(i =>
+                    TestHelpers
+                        .PrimeFactors(i)
+                        .Select(Convert.ToInt32)
+                        .GroupBy(f => f)
+                        .Select(g => new KeyValuePair<int, int>(g.Key, g.Count())))
+                .GroupBy(kvp => kvp.Key)
+                .Select(g => (g.Key, g.Max(x => x.Value)))
+                .Aggregate(1, (x, y) =>
+                    x * Enumerable.Range(0, y.Item2)
+                        .Select(_ => y.Key)
+                        .Aggregate(1, (m, n) => m * n));
+
+            Assert.That(answer, Is.EqualTo(expected));
+        }
+        
+        
+    }
+}
