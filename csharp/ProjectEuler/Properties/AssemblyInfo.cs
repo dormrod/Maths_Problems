0aeb5a8d70e17bca1b6781f2c7515975b94aca95
David Ormrod Morley <david.ormrodmorley@finbourne.com>
2022-06-29 22:17:23 +0100

    Rename solution

--- csharp/ProjectEuler/Properties/AssemblyInfo.cs
+++ csharp/ProjectEuler/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ProjectEuler.UnitTests")]
+[assembly: AssemblyTitle("ProjectEuler")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ProjectEuler.UnitTests")]
+[assembly: AssemblyProduct("ProjectEuler")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
